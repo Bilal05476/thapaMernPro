afc8258d8b0f058a04c059c2d53634d477287fd1
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-04-23 00:20:18 +0500

    updates

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
+require("../DB/conn");
+const User = require("../model/userSchema");
+
 router.get("/", (req, res) => {
   res.send("Hello World from Server");
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.json({message: req.body})
-})
+router.post("/register", (req, res) => {
+  const { name, email, phone, work, password, cPassword } = req.body;
+  if (!name || !email || !phone || !work || !password || !cPassword) {
+    return res.status(422).json({ error: "Plz enter all fields" });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "Email already Exist" });
+      }
+      const user = new User({ name, email, phone, work, password, cPassword });
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "Successfully Registered" });
+        })
+        .catch((err) => res.status(500).json({ error: "Failed Registration" }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
