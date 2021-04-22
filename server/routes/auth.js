0df3ebbded81d4f3cc084e0b62262c5c0b46a389
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-04-23 00:38:30 +0500

    updates

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,29 +8,55 @@ router.get("/", (req, res) => {
   res.send("Hello World from Server");
 });
 
-router.post("/register", (req, res) => {
+//Promises Method, Callback Hell
+// router.post("/register",(req, res) => {
+//   const { name, email, phone, work, password, cPassword } = req.body;
+//   if (!name || !email || !phone || !work || !password || !cPassword) {
+//     return res.status(422).json({ error: "Plz enter all fields" });
+//   }
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "Email already Exist" });
+//       }
+//       const user = new User({ name, email, phone, work, password, cPassword });
+
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "Successfully Registered" });
+//         })
+//         .catch((err) => res.status(500).json({ error: "Failed Registration" }));
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+//Async, await Method
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cPassword } = req.body;
+
   if (!name || !email || !phone || !work || !password || !cPassword) {
     return res.status(422).json({ error: "Plz enter all fields" });
   }
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email already Exist" });
+    }
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "Email already Exist" });
-      }
-      const user = new User({ name, email, phone, work, password, cPassword });
+    const user = new User({ name, email, phone, work, password, cPassword });
+    const userRegister = await user.save();
 
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "Successfully Registered" });
-        })
-        .catch((err) => res.status(500).json({ error: "Failed Registration" }));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    if (userRegister) {
+      res.status(201).json({ message: "Successfully Registered" });
+    } else {
+      res.status(500).json({ error: "Failed Registration" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
